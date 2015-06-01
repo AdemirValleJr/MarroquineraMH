1fe046c5147f0b3694a8e2862627b8ac66973707
silverio.bernal@orkidea.co <silverio.bernal@orkidea.co>
2015-06-01 18:55:59 -0500

    Biz Reporte

--- ReporteZ/OrkIdea.MH.ReporteZ.Business/BizReporteZ.cs
+++ ReporteZ/OrkIdea.MH.ReporteZ.Business/BizReporteZ.cs
@@ -3,6 +3,7 @@ using CrystalDecisions.Shared;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 //using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace OrkIdea.MH.ReporteZ.Business
     public static class BizReporteZ
     {
         public static void ImprimirReporte(string tienda, DateTime fecha, string terminal, string impresora)
-        {           
+        {
 
             string oConnStr = ConfigurationManager.ConnectionStrings["Linx"].ToString();
             System.Data.SqlClient.SqlConnectionStringBuilder oConnBuilder = new System.Data.SqlClient.SqlConnectionStringBuilder(oConnStr);
@@ -25,44 +26,90 @@ namespace OrkIdea.MH.ReporteZ.Business
 
             using (ReportDocument rpt = new ReportDocument())
             {
-                string rutaRpt = AppDomain.CurrentDomain.BaseDirectory + @"\z.rpt";
-
-                rpt.Load(rutaRpt);
-
-                ParameterDiscreteValue TiendaDiscreteValue = new ParameterDiscreteValue();
-                TiendaDiscreteValue.Value = tienda;
-                rpt.SetParameterValue("Tienda", TiendaDiscreteValue);
-
-                ParameterDiscreteValue fechaDiscreteValue = new ParameterDiscreteValue();
-                fechaDiscreteValue.Value = fecha;
-                rpt.SetParameterValue("fecha", fechaDiscreteValue);
-
-                ParameterDiscreteValue TerminalDiscreteValue = new ParameterDiscreteValue();
-                TerminalDiscreteValue.Value = terminal;
-                rpt.SetParameterValue("Terminal", TerminalDiscreteValue);
-
-                Tables tables = rpt.Database.Tables;
-                foreach (CrystalDecisions.CrystalReports.Engine.Table table in tables)
+                try
                 {
-                    CrystalDecisions.Shared.TableLogOnInfo tableLogonInfo = table.LogOnInfo;
-                    tableLogonInfo.ConnectionInfo = connectionInfo;
-                    table.ApplyLogOnInfo(tableLogonInfo);
-                }
+                    string rutaRpt = AppDomain.CurrentDomain.BaseDirectory + @"\z.rpt";
 
-                for (int i = 0; i < rpt.DataSourceConnections.Count; i++)
+                    rpt.Load(rutaRpt);
+
+                    ParameterDiscreteValue TiendaDiscreteValue = new ParameterDiscreteValue();
+                    TiendaDiscreteValue.Value = tienda;
+                    rpt.SetParameterValue("Tienda", TiendaDiscreteValue);
+
+                    ParameterDiscreteValue fechaDiscreteValue = new ParameterDiscreteValue();
+                    fechaDiscreteValue.Value = fecha;
+                    rpt.SetParameterValue("fecha", fechaDiscreteValue);
+
+                    ParameterDiscreteValue TerminalDiscreteValue = new ParameterDiscreteValue();
+                    TerminalDiscreteValue.Value = terminal;
+                    rpt.SetParameterValue("Terminal", TerminalDiscreteValue);
+
+                    Tables tables = rpt.Database.Tables;
+                    foreach (CrystalDecisions.CrystalReports.Engine.Table table in tables)
+                    {
+                        CrystalDecisions.Shared.TableLogOnInfo tableLogonInfo = table.LogOnInfo;
+                        tableLogonInfo.ConnectionInfo = connectionInfo;
+                        table.ApplyLogOnInfo(tableLogonInfo);
+                    }
+
+                    for (int i = 0; i < rpt.DataSourceConnections.Count; i++)
+                    {
+                        rpt.DataSourceConnections[i].SetConnection(oConnBuilder.DataSource, oConnBuilder.InitialCatalog, oConnBuilder.UserID, oConnBuilder.Password);
+                    }
+
+                    rpt.SetDatabaseLogon(oConnBuilder.UserID, oConnBuilder.Password, oConnBuilder.DataSource, oConnBuilder.InitialCatalog);
+
+
+                    //rpt.PrintOptions.PrinterName = impresora;
+                    //rpt.PrintToPrinter(1, false, 0, 0);
+
+                    Console.WriteLine("Great you did it!");
+                    //Console.Read();
+
+
+                    // export to pdf
+                    string pdfFile = string.Format(@"{0}\tmp\reporteZ-{1}-{2}.pdf", AppDomain.CurrentDomain.BaseDirectory, tienda, terminal);
+
+                    ExportOptions CrExportOptions;
+                    DiskFileDestinationOptions CrDiskFileDestinationOptions = new DiskFileDestinationOptions();
+                    PdfRtfWordFormatOptions CrFormatTypeOptions = new PdfRtfWordFormatOptions();
+                    CrDiskFileDestinationOptions.DiskFileName = pdfFile;
+                    CrExportOptions = rpt.ExportOptions;
+                    {
+                        CrExportOptions.ExportDestinationType = ExportDestinationType.DiskFile;
+                        CrExportOptions.ExportFormatType = ExportFormatType.PortableDocFormat;
+                        CrExportOptions.DestinationOptions = CrDiskFileDestinationOptions;
+                        CrExportOptions.FormatOptions = CrFormatTypeOptions;
+                    }
+                    rpt.Export();
+
+
+                    Process.Start(pdfFile);
+
+                    //print pdf
+                    //ProcessStartInfo info = new ProcessStartInfo();
+                    //info.Verb = "print";
+                    //info.FileName = pdfFile;
+                    //info.CreateNoWindow = true;
+                    //info.WindowStyle = ProcessWindowStyle.Hidden;
+                    //info.Arguments = "\"" + impresora + "\"";
+                    //info.UseShellExecute = true;
+                    //info.Verb = "PrintTo";
+
+                    //Process p = new Process();
+                    //p.StartInfo = info;
+                    //p.Start();
+
+                    //p.WaitForInputIdle();
+                    //System.Threading.Thread.Sleep(3000);
+                    //if (false == p.CloseMainWindow())
+                    //    p.Kill();
+                }
+                catch (Exception ex)
                 {
-                    rpt.DataSourceConnections[i].SetConnection(oConnBuilder.DataSource, oConnBuilder.InitialCatalog, oConnBuilder.UserID, oConnBuilder.Password);
+                    Console.WriteLine(ex.ToString());
+                    Console.Read();
                 }
-
-                rpt.SetDatabaseLogon(oConnBuilder.UserID, oConnBuilder.Password, oConnBuilder.DataSource, oConnBuilder.InitialCatalog);
-
-
-                rpt.PrintOptions.PrinterName = impresora;
-
-                rpt.PrintToPrinter(1, false, 0, 0);
-
-                //Console.WriteLine("Great you did it!");
-                //Console.Read();
             }
         }
     }
